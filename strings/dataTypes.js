370c3426c3b0a2897ea44fbaadccde0bb24a49f9
Lokesh kumar singh <lokeshbsr544@gmail.com>
2026-01-07 17:02:22 +0530

    Day 2: JavaScript data types

---
+++ strings/dataTypes.js
@@ -0,0 +1,19 @@
+// Primitive Data Types
+let name = "Lokesh"; // String
+let age = 22; // Number
+let isDeveloper = true; // Boolean
+let x = undefined;
+let y = null;
+
+// Non-Primitive Data Types
+let skills = ["JavaScript", "React", "Node"];
+let user = {
+  name: "Lokesh",
+  role: "MERN Developer",
+};
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isDeveloper);
+console.log(typeof skills);
+console.log(typeof user);
