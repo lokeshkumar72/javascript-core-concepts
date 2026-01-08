f1636d0afe581de15c34d640d874cc09a21391fd
Lokesh kumar singh <lokeshbsr544@gmail.com>
2026-01-08 16:15:56 +0530

    Day 5: Strings and objects in JavaScript

---
+++ strings/stringObjects.js
@@ -0,0 +1,18 @@
+// String examples
+let message = "Hello JavaScript";
+
+console.log(message.length);
+console.log(message.toUpperCase());
+console.log(message.includes("JavaScript"));
+console.log(message.replace("JavaScript", "World"));
+
+// Object examples
+let user = {
+  name: "Lokesh",
+  role: "MERN Developer",
+  skills: ["JS", "React", "Node"],
+};
+
+console.log(user.name);
+console.log(user.role);
+console.log(user.skills.join(", "));
